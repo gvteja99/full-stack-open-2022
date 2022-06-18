33ed63de3b5ae3ed34de06f5ed7ee8afec06e283
gvteja99 <gvteja99@gmail.com>
2022-06-18 12:12:19 -0700

    Finish exercise 2.1 in 'Rendering a collection, modules'

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,6 @@
-const Hello = (props) => (
+const Header = (props) => (
   <div>
-    <h1>{props.course}</h1>
+    <h1>{props.name}</h1>
   </div>)
 
 const Part = (props) => (
@@ -23,32 +23,40 @@ const Total = (props) => (
   </div>
 )
 
+
+const Course = (props) => {
+  return (<div>
+            <Header name={props.course.name}/>
+            <Content parts={props.course.parts}/>
+          </div>)
+}
+
+
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Hello course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
+  return <Course course={course} />
 }
+
 
 export default App
