57e1b0c9621620282829189d84a47c7fbcbc9075
gvteja99 <gvteja99@gmail.com>
2022-06-17 21:17:03 -0700

    Finish exercise 1.4 in JavaScript

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -10,7 +10,6 @@ const Part = (props) => (
 )
 
 const Content = (props) => {
-  console.log(props)
   return (<div>
     <Part part = {props.parts[0].name} exercise={props.parts[0].exercises} />
     <Part part = {props.parts[1].name} exercise={props.parts[1].exercises} />
@@ -26,20 +25,20 @@ const Total = (props) => (
 
 const App = () => {
   const course = 'Half Stack application development'
-    const part1 = {
+  const parts = [
+    {
       name: 'Fundamentals of React',
       exercises: 10
-    }
-    const part2 = {
+    },
+    {
       name: 'Using props to pass data',
       exercises: 7
-    }
-    const part3 = {
+    },
+    {
       name: 'State of a component',
       exercises: 14
     }
-
-    const parts = [part1, part2, part3]
+  ]
 
   return (
     <div>
