d1ddbd78ee138903f8d4424feb8472c2b5d7257a
gvteja99 <gvteja99@gmail.com>
2022-06-18 12:24:41 -0700

    Finish exercise 2.5 in 'Rendering a collection, modules'

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,37 +1,4 @@
-const Header = (props) => (
-  <div>
-    <h2>{props.name}</h2>
-  </div>)
-
-const Part = (props) => (
-  <div>
-    <p>{props.part} {props.exercise}</p>
-  </div>
-)
-
-const Content = (props) => {
-  return (<div>
-    {props.parts.map( part => <Part part = {part.name} exercise={part.exercises} />)}
-  </div>)
-}
-
-const Total = (props) => {
-  const total = props.parts.reduce((partialSum, a) => partialSum + a.exercises, 0)
-  return(
-  <div>
-    <p><b>Number of exercises {total}</b></p>
-  </div>
-  )
-}
-
-const Course = (props) => {
-  return (<div>
-            <Header name={props.course.name}/>
-            <Content parts={props.course.parts}/>
-            <Total parts={props.course.parts}/>
-          </div>)
-}
-
+import Course from './Course'
 
 const App = () => {
   const courses = [
