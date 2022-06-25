7b0a07d5eafd30b0440315886624d7840d3e7eef
gvteja99 <gvteja99@gmail.com>
2022-06-25 13:07:40 -0700

    Fix the empty contacts section issue

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,4 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -7,23 +6,10 @@ import Persons from './components/Persons'
 const App = () => {
   const [persons, setPersons] = useState([])
 
-  const hook = () => {
-    console.log('effect')
-    axios.get('http://localhost:3001/persons').then(response => {
-                          const notes = response.data
-                          setPersons(notes);
-                          console.log(notes)
-                        })
-      
-  }
-  
-  useEffect(hook, [])
-
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
-  console.log('persons', persons)
 
   return (
     <div>
@@ -39,7 +25,7 @@ const App = () => {
           setNewNumber={setNewNumber}
         />
       <h2>Numbers</h2>
-        <Persons persons={persons} />
+        <Persons persons={persons} setPersons={setPersons} newFilter={newFilter} />
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,10 +1,36 @@
 import Name from './Name'
+import axios from 'axios'
+import { useEffect } from 'react'
 
-const Persons = ({persons}) => {
-    return (<div>
-      {persons.filter(person => person.show).map(person => <Name key={person.id} name={person.name} number={person.number} />)}
-      </div>
-      )
+
+
+
+const Persons = ({persons, setPersons, newFilter}) => {
+
+  const hook = () => {
+    console.log('effect')
+    axios.get('http://localhost:3001/persons').then(response => {
+                          const notes = response.data
+                          setPersons(notes);
+                          console.log(notes)
+                        })
+      
   }
+  
+  useEffect(hook, [])
+  
+  if (persons.length==0) {
+    return (<div></div>)
+  }
+
+  console.log('persons', persons)
+
+  return (<div>
+    {persons
+    .filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+    .map(person => <Name key={person.id} name={person.name} number={person.number} />)}
+    </div>
+    )
+}
 
 export default Persons
