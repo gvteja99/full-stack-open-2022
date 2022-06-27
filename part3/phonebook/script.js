e551f58fa08b816d255dc87fab10c40d4c2d0c4b
gvteja99 <gvteja99@gmail.com>
2022-06-26 17:30:16 -0700

    Finish exercise 3.2 in 'Node.js and Express'

--- part3/phonebook/script.js
+++ part3/phonebook/script.js
@@ -36,6 +36,9 @@ app.get('/', (request, response) => {
   response.send('<h1>Hellowow World!</h1>')
 })
 
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people<\p><p>${new Date()}<\p>`)
+})
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
