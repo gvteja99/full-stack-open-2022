7f2d3db94e00e2f5d472efbdf028fac031338192
gvteja99 <gvteja99@gmail.com>
2022-06-30 22:25:48 -0700

    Finish exercises 4.2 in 'Structure of backend application, introduction to testing'

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,12 +1,10 @@
 const config = require('./utils/config')
-const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
 const blogsRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI)
@@ -19,7 +17,12 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
