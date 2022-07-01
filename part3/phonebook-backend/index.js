0db383615ea65626260741d891c6a59790e7c133
gvteja99 <gvteja99@gmail.com>
2022-06-30 17:38:34 -0700

    Finish exercises 3.15-3.18 in 'Saving data to MongoDB'

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,118 +1,128 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 const cors = require("cors")
 var morgan = require('morgan')
+const mongoose = require('mongoose')
 
-// const corsOptions = { credentials: true, origin: "*" }
-
+app.use(express.static('build'))
 app.use(cors())
+app.use(express.json())
 
+
+const Person = require('./models/person')
+
+var contacts = [];
 
 morgan.token('body', function getId (req) {
-  // console.log(JSON.stringify(obj))
   return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-
-let persons = [
-                { 
-                    "id": 1,
-                    "name": "Arto Hellas", 
-                    "number": "040-123456"
-                },
-                { 
-                    "id": 2,
-                    "name": "Ada Lovelace", 
-                    "number": "39-44-5323523"
-                },
-                { 
-                    "id": 3,
-                    "name": "Dan Abramov", 
-                    "number": "12-43-234345"
-                },
-                { 
-                    "id": 4,
-                    "name": "Mary Poppendieck", 
-                    "number": "39-23-6423122"
-                }
-            ]
-
-app.use(express.json())
-app.use(express.static('build'))
-
 app.get('/', (request, response) => {
   response.send('<h1>Hellowow World!</h1>')
 })
 
+
 app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people<\p><p>${new Date()}<\p>`)
+  response.send(`<p>Phonebook has info for ${contacts.length} people<\p><p>${new Date()}<\p>`)
 })
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+
+app.get('/api/persons', (request, response, next) => {
+  Person.find({}).then(persons => {
+    contacts = persons;
+    return response.json(persons)
+    }).catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
     return response.status(400).json({ 
       error: 'field missing' 
     })
-  } else if (persons.filter(person => person.name === body.name).length != 0) {
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedNote => {
+    response.json(savedNote)
+  }).catch(error => next(error))
+
+})
+
+
+app.get('/api/persons/:id', (request, response, next) => {
+    // const id = request.params.id
+    // const person = contacts.find(person => person.id === id)
+    // person ? response.json(person) : response.status(404).end()
+
+    Person.findById(request.params.id)
+    .then(person => {if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }}).catch(error => next(error))
+
+})
+
+app.put('/api/persons/:id', (request, response) => {
+  const body = request.body
+
+  console.log(body)
+
+  if (!body.name || !body.number) {
     return response.status(400).json({ 
-      error: 'name must be unique' 
+      error: 'field missing' 
     })
   }
 
   const person = {
-    id: body.id,
     name: body.name,
     number: body.number,
   }
 
-  persons = persons.concat(person)
+  Person.findByIdAndUpdate(body.id, person, {new: true})
+  .then(updatedNote => {
+    response.json(updatedNote)
+  })
+  .catch(error => next(error))
 
-  response.json(body)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    person ? response.json(person) : response.status(404).end()
-})
 
-// app.put('/api/persons/:id', (request, response) => {
-//   const body = request.body
+app.delete('/api/persons/:id', (request, response, next) => {
+    const doc_id = request.params.id
+    Person.findOneAndRemove({_id: doc_id}).then(person => {if (person){
+                                                              person.remove()
+                                                              return response.status(204).end()
+                                                            } else {
+                                                              return response.status(403).end()
+                                                            }}).catch(error => next(error))
 
-//   console.log(body)
-
-//   if (!body.name || !body.number) {
-//     return response.status(400).json({ 
-//       error: 'field missing' 
-//     })
-//   }
-
-//   const person = {
-//     id: body.id,
-//     name: body.name,
-//     number: body.number,
-//   }
-
-//   persons[body.id-1] = person
-//   response.json(body)
-// })
-
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
   
-    response.status(204).end()
   })  
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+  
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- part3/phonebook/src/components/PersonForm.js
+++ part3/phonebook/src/components/PersonForm.js
@@ -3,15 +3,15 @@ import { useEffect } from 'react'
 
 const PersonForm = ({newName, newNumber, persons, setPersons, setNewName, setNewNumber, setNotificationMessage, setToggle}) => {
 
-    // const containsObject = (obj, list) => {
-    //   var i;
-    //   for (i = 0; i < list.length; i++) {
-    //       if (list[i]["name"] === obj["name"]) {
-    //         return list[i]["number"] === obj["number"]?"exists":list[i].id;
-    //       }           
-    //   }
-    //   return "add";
-    // }
+    const containsObject = (obj, list) => {
+      var i;
+      for (i = 0; i < list.length; i++) {
+          if (list[i]["name"] === obj["name"]) {
+            return list[i]["number"] === obj["number"]?"exists":list[i].id;
+          }           
+      }
+      return "add";
+    }
 
     let toggle = false;
   
@@ -25,37 +25,37 @@ const PersonForm = ({newName, newNumber, persons, setPersons, setNewName, setNew
         number: newNumber,
       }
  
-      // const hasObject = containsObject(nameObject, persons)
-      // if (hasObject === "exists") { 
-      //   alert(`${newName} with the number ${newNumber} is already added to the phonebook`)
-      // } else if (hasObject === "add") {
-      contactServices.createContact(nameObject).then(contacts => {
-        setPersons(persons.concat(contacts))
-        setNewName('')
-        setNewNumber('')
+      const hasObject = containsObject(nameObject, persons)
+      if (hasObject === "exists") { 
+        alert(`${newName} with the number ${newNumber} is already added to the phonebook`)
+      } else if (hasObject === "add") {
+        contactServices.createContact(nameObject).then(contacts => {
+          setPersons(persons.concat(contacts))
+          setNewName('')
+          setNewNumber('')
 
-        setNotificationMessage(`Added ${newName}`)
+          setNotificationMessage(`Added ${newName}`)
 
-        setTimeout(() => {
-          setNotificationMessage('')
-        }, 3000)
+          setTimeout(() => {
+            setNotificationMessage('')
+          }, 3000)
 
-      })
-      // } else {
-        // if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)) {
-        //   contactServices.updateContact({ ...nameObject, id: hasObject}).then(contact => {
-        //     setPersons(persons.map(person => person.id !== hasObject ? person : contact))
-        //     setNewName('')
-        //     setNewNumber('')  
+        })
+      } else {
+        if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)) {
+          contactServices.updateContact({ ...nameObject, id: hasObject}).then(contact => {
+            setPersons(persons.map(person => person.id !== hasObject ? person : contact))
+            setNewName('')
+            setNewNumber('')  
             
-        //     setNotificationMessage(`Updated ${newName}`)
+            setNotificationMessage(`Updated ${newName}`)
 
-        //     setTimeout(() => {
-        //       setNotificationMessage('')
-        //     }, 3000)
-        //   })
-        // }
-      // } 
+            setTimeout(() => {
+              setNotificationMessage('')
+            }, 3000)
+          })
+        }
+      } 
       setToggle(true);
     }
   
