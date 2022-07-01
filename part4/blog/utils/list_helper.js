f490a46ec2b24279aa6ada13aba72633c16957dd
gvteja99 <gvteja99@gmail.com>
2022-06-30 23:13:32 -0700

    Finish exercises 4.5 in 'Structure of backend application, introduction to testing'

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -4,7 +4,9 @@ const totalLikes = (obj) => obj.reduce(function (previousValue, currentValue) {
     return previousValue + currentValue.likes
   }, 0)
 
+const favoriteBlog = (obj) => obj.reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
   }  
   
