3a1697c2eaa451ee7f44740d12e5a89773f8f037
gvteja99 <gvteja99@gmail.com>
2022-06-30 22:18:24 -0700

    Refactor code to add a controller module

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blog')
 
 const mongoose = require('mongoose')
 
@@ -17,24 +18,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
